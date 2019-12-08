17a09ba656cedcabe8a3507676485c26f2ff0695
Tim Collart <timc@vliz.be>
2019-12-08 09:45:39 +0100

    adding the scripts

---
+++ PCA_and_kmeans/scripts/grablegend.R
@@ -0,0 +1,6 @@
+#function to grab the legend of a ggplot pbject
+g_legend<-function(a.gplot){
+        tmp <- ggplot_gtable(ggplot_build(a.gplot))
+        leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
+        legend <- tmp$grobs[[leg]]
+        return(legend)}
